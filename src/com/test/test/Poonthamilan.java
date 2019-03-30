9fc125cd4a87312d3e305410614fc9da527f0a63
poonthamilan0818 <poonthamilan0818@gmail.com>
2019-03-30 13:16:12 +0530

    my class is added

---
+++ src/com/test/test/Poonthamilan.java
@@ -0,0 +1,11 @@
+package com.test.test;
+
+public class Poonthamilan {
+	public static void main(String[] args) {
+		String ab = "CLONING A PROJECT A FROM  A REPOSITORY A TO LOCAL";
+		String case1 = ab.toLowerCase();
+		System.out.println(case1);
+		int i = case1.indexOf("a");
+		System.out.println(i);
+	}
+}
