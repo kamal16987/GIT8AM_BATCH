13d997a9fb465a955e9187ed72f679399a05b860
Thameem1806 <thameemansari1806.com>
2019-03-31 23:29:10 +0530

    Project class

---
+++ Thameem.java
@@ -0,0 +1,19 @@
+
+public class Thameem {
+	public static void main(String[] args) {
+		String str = "Project class started";
+		char[] ch = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			int count = 0;
+			char charAt = str.charAt(i);
+			for (int j = 0; j < ch.length; j++) {
+				if (charAt == ch[j]) {
+					count++;
+				}
+			}
+			if (count > 0) {
+				System.out.println(charAt + "=" + count);
+			}
+		}
+	}
+}
