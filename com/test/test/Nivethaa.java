447230555608701f69899b45779c07c0f1e19373
Nivethaa <nivethaasrini@gmail.com>
2019-04-01 00:31:28 +0530

    Added Count of Character in string

---
+++ com/test/test/Nivethaa.java
@@ -0,0 +1,31 @@
+package com.test.test;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Nivethaa {
+	private void countCharInString(String s) {
+		HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+		char[] ch = s.toCharArray();
+		for (char c : ch) {
+			if (charCount.containsKey(c)) {
+				charCount.put(c, charCount.get(c) + 1);
+			} else {
+				charCount.put(c, 1);
+			}
+		}
+		Set<Entry<Character, Integer>> es = charCount.entrySet();
+		for (Entry<Character, Integer> i : es) {
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string : ");
+		String str = sc.nextLine();
+		Nivethaa co = new Nivethaa();
+		co.countCharInString(str);
+	}
+}
