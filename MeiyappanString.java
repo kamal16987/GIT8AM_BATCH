268a875ffd4530428bdf54052ae56a5fbd674b09
Meiyappan1995 <meiyappanp95@gmail.com>
2019-03-31 14:20:22 +0530

    String class cloned

---
+++ MeiyappanString.java
@@ -0,0 +1,19 @@
+package com.test.test;
+public class MeiyappanString {
+	public static void main(String[] args) {
+		String str = "cloaning has successful";
+		char[] ch = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			int count = 0;
+			char charAt = str.charAt(i);
+			for (int j = 0; j < ch.length; j++) {
+				if (charAt == ch[j]) {
+					count++;
+				}
+			}
+			if (count > 0) {
+				System.out.println(charAt + "=" + count);
+			}
+		}
+	}
+}
