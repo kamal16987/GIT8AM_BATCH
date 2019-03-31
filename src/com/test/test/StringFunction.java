6024fc947fa8f4830fe16cf1fd36039f8feaa7c2
sthillaiarasu <thillaiarasu.s@gmail.com>
2019-03-31 11:08:16 +0530

    string function

---
+++ src/com/test/test/StringFunction.java
@@ -0,0 +1,20 @@
+package com.test.test;
+
+public class StringFunction {
+
+	public static void main(String[] args) {
+
+		String s1 = "hi my name is";
+		int count = 0;
+		for (int i = 0; i < s1.length(); i++) {
+			char ch = s1.charAt(i);
+			if (ch == 'a') {
+				count = count + 1;
+
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
