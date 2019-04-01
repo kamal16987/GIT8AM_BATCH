d1e1a833ab68c070df91ae0e96c3f1eabfe68565
Thameem1806 <thameemansari1806.com>
2019-04-01 20:31:11 +0530

    Thameem_day3 main class added

---
+++ com/test/test/Thameem_day3.java
@@ -0,0 +1,20 @@
+package com.test.test;
+
+public class Thameem_day3 {
+	public static void main(String[] args) {
+		String str = "Project class started";
+		char[] ch = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			int count = 0;
+			char charAt = str.charAt(i);
+			for (int j = 0; j < ch.length; j++) {
+				if (charAt == ch[j]) {
+					count++;
+				}
+			}
+			if (count > 0) {
+				System.out.println(charAt + "=" + count);
+			}
+		}
+	}
+}
