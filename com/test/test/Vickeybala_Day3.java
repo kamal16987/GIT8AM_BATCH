0d1d919775eb7a5730b7fa445e1a85fe74ee5db1
unknown <vickeybala17@gmail.com>
2019-04-01 11:07:12 +0530

    Day 3 Task has been Completed

---
+++ com/test/test/Vickeybala_Day3.java
@@ -0,0 +1,25 @@
+package com.test.test;
+
+public class Vickeybala_Day3 {
+	public static void main(String[] args) 
+	{
+		String s = "Vickey Bala is a good boy";
+		char[] c = s.toCharArray();
+		for (int i = 0; i < c.length; i++)
+		{
+			int count = 0;
+			char charAt = s.charAt(i);
+			for (int j = 0; j < c.length; j++)
+			{
+				if (charAt == c[j])
+				{
+					count++;
+				}
+			}
+			if (count > 0) {
+				System.out.println(charAt + "=" + count);
+			}
+		}
+	}
+
+}
