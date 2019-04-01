8c782b55fcfc367d6fb95b170523fb38f886b038
Manikandan1294 <manismart1294@gmail.com>
2019-04-01 10:07:52 +0530

    add the class

---
+++ com/test/test/Mani12.java
@@ -0,0 +1,16 @@
+package com.test.test;
+
+public class Mani12 {
+	public static void main(String[] args) {
+		String a=("hi my class");
+		int b=0;
+		for (int i = 0; i < a.length(); i++) {
+			char name=a.charAt(i);
+			if (name =='a') {
+				b=b+1;
+				
+	}
+			System.out.println(b);
+		}
+	}
+}
