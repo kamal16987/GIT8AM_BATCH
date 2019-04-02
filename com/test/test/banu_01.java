9b80d51788de6260c8146f747c4a3a7fdf971f56
banu <bowsiya22a@gmail.com>
2019-04-02 10:14:19 +0530

    new class created

---
+++ com/test/test/banu_01.java
@@ -0,0 +1,16 @@
+package com.test.test;
+
+public class banu_01 {
+
+		public static void main(String[] args) {
+			int a =101;
+			if(a%2==0){
+				System.out.println("even");
+			}
+			else{
+				System.out.println("odd");
+			}
+		
+	}
+
+}
