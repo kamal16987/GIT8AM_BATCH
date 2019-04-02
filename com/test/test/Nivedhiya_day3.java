036ba369bc0961c2178f3e3fa555186116f7ff1d
Nivedhiyasiva <nvdhponnuswamy6@gmail.com>
2019-04-01 23:49:59 -0700

    added merge conflict

--- com/test/test/Nivedhiya_day3.java
+++ com/test/test/Nivedhiya_day3.java
@@ -2,15 +2,25 @@ package com.test.test;
 
 public class Nivedhiya_day3 {
 
-	public void Status() {
-
-		System.out.println("New method added in Nivedhiya_02");
-
-	}
 
 	
 			
 		
 	
+
+
+	public static void main(String[] args) {
+		System.out.println("Test cases Added");
+	}
+
+	public void Testcase_03() {
+		
+		System.out.println("Test case_03 has been added");
+	}
+
+	public void Testcase_02() {
+		
+		System.out.println("Test case_03 has been added");
+	}
 
 }
