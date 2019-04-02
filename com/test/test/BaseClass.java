1e599b30b926ca6a07dc71b1f0886a584745d51a
sathishkumar786 <sam.pearl786@gmail.com>
2019-04-02 15:25:06 +0530

    Baseclass small modification

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -2,16 +2,21 @@ package com.test.test;
 
 public class BaseClass {
 	
-	public void method()
+	public void GitProcess()
 	{
-		System.out.println("hello");
+		System.out.println("Baseclass changed");
 	}
 
+	public void Gitprocess1()
+	{
+		System.out.println("Modification verified");
+	}
 	public static void main(String[] args) {
 		
-BaseClass bd=new BaseClass();
-		bd.method();
-	System.out.println("modified");	
+BaseClass bc=new BaseClass();
+		bc.GitProcess();
+		bc.Gitprocess1();
+	System.out.println("Done");	
 
 }
 
