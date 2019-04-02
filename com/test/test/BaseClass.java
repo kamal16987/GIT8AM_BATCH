0d99163a5431f4984498e41e497cb746e05ae965
Your Name <MadhuAV2812>
2019-04-02 11:15:57 +0530

    Day_04 task completed

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -3,30 +3,7 @@ package com.test.test;
 public class BaseClass {
 
 	public static void main(String[] args) {
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
+			System.out.println("Created a Baseclass...");
 		System.out.println("Created a Baseclass...");
 		System.out.println("Created a Baseclass...");
 		System.out.println("Created a Baseclass...");
--- com/test/test/Day_02.java
+++ com/test/test/Day_02.java
@@ -40,7 +40,7 @@ public class Day_02 {
 
 
 
-	}
+	
 	}
 
-}
+
