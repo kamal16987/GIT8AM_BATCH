cd99835915d78c6097646274f41b6094d4f65336
Nivedhiyasiva <nvdhponnuswamy6@gmail.com>
2019-04-01 23:20:45 -0700

    Base class modified

--- com/test/test/Nivedhiya_day3.java
+++ com/test/test/Nivedhiya_day3.java
@@ -1,10 +1,17 @@
 package com.test.test;
 
 public class Nivedhiya_day3 {
+	public static void main(String[] args) {
+		System.out.println("Test cases Added");
+	}
 
 	public void Testcase_03() {
 		
 		System.out.println("Test case_03 has been added");
 	}
 
+	public void Testcase_02() {
+		
+		System.out.println("Test case_03 has been added");
+	}
 }
