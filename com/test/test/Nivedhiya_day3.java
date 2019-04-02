8ac087a4ad93f548e759580c039e78f3634d4760
Nivedhiyasiva <nvdhponnuswamy6@gmail.com>
2019-04-01 23:16:25 -0700

    Added Nivedhiya_Testcase_03 in method

--- com/test/test/Nivedhiya_day3.java
+++ com/test/test/Nivedhiya_day3.java
@@ -2,9 +2,9 @@ package com.test.test;
 
 public class Nivedhiya_day3 {
 
-	public static void main(String[] args) {
-
-		System.out.println("Added branch Nivedhiya_01");
+	public void Testcase_03() {
+		
+		System.out.println("Test case_03 has been added");
 	}
 
 }
