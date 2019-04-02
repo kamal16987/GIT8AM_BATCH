6d56617b5f37c5817baca8c63d0c4dc816a02b8c
banu <bowsiya22a@gmail.com>
2019-04-02 10:22:18 +0530

    banu001 added

---
+++ com/test/test/banu001.java
@@ -0,0 +1,17 @@
+package com.test.test;
+
+public class banu001 {
+	public void comID(){
+		System.out.println("id is 23847");
+
+		
+	}
+	public void comName(){
+		System.out.println("comname is CTS");
+	}
+	public static void main(String[] args) {
+		banu001 ci = new banu001();
+		ci.comID();
+		ci.comName();
+}
+}
