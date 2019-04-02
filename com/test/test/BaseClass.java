82f0b5157f6bd8463a845f7cc3d2beb7cd96f06b
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-04-02 08:40:52 +0530

    Modified BaseClass

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -3,8 +3,12 @@ package com.test.test;
 public class BaseClass {
 
 	public static void main(String[] args) {
-		System.out.println("Created a Baseclass...");
-
+		System.out.println("I am going to modify the baseclass.");
+		//WebDriver e = new chromeDriver();		
+	}
+	
+	public void setValueInTextBox() {
+		
 	}
 
 }
