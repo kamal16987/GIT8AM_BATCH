400aac06c468ec90c21e377438959d7f461f7338
sthillaiarasu <thillaiarasu.s@gmail.com>
2019-04-02 12:22:39 +0530

    conflict

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -1,13 +1,18 @@
 package com.test.test;
 
 public class BaseClass {
+	
+	public void method()
+	{
+		System.out.println("hello");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Created a Baseclass...");
 		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
-		System.out.println("Created a Baseclass...");
+		
+		BaseClass bd=new BaseClass();
+		bd.method();
 	
 
 }
