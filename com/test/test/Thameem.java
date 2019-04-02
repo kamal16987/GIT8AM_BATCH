878e58e046bc4f191488d9d6aafe0965bbe5e8cb
poonthamilan0818 <poonthamilan0818@gmail.com>
2019-04-02 20:55:35 +0530

    modified this class

--- com/test/test/Thameem.java
+++ com/test/test/Thameem.java
@@ -3,18 +3,14 @@ package com.test.test;
 public class Thameem {
 	public static void main(String[] args) {
 		String str = "Project class was started";
-		char[] ch = str.toCharArray();
-		for (int i = 0; i < ch.length; i++) {
-			int count = 0;
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
 			char charAt = str.charAt(i);
-			for (int j = 0; j < ch.length; j++) {
-				if (charAt == ch[j]) {
-					count++;
-				}
+			if (charAt == 's') {
+				count++;
 			}
-			if (count > 0) {
-				System.out.println(charAt + "=" + count);
-			}
+			
 		}
+		System.out.println("count of s:"+count);
 	}
 }
