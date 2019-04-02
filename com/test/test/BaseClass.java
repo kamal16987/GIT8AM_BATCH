f2ceffe774fbff65435c5f897aad9594a10a4367
Nandhini13panduranga <pjnandhini25@gmail.com>
2019-04-02 09:55:30 +0530

    clickBtn method added

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -5,25 +5,29 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("Created a Baseclass...");
 		System.out.println("I am going to modify the baseclass.");
-		//WebDriver e = new chromeDriver();		
+		// WebDriver e = new chromeDriver();
 	}
-	
+
 	public void setValueInTextBox() {
-		
+
 	}
-		
+
+	public void clickBtn() {
+		System.out.println("Added clickBtn method");
+	}
+
 	public BaseClass() {
-		
+
 	}
-	
+
 	public void launchBrowser() {
 		System.out.println("Added launch browser method");
 	}
-	
+
 	public void selectDropdown() {
 		System.out.println("Added select Dropdown method");
 	}
-	
+
 	public void selectCheckbox() {
 		System.out.println("Added select Checkbox method");
 	}
