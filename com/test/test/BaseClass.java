576a0d00ac08f12ae98cce8fe4851852be4b7bbf
Sharief1807 <sharief.indian@gmail.com>
2019-04-02 14:48:49 +0530

    baseclass modified with sum of individual numbers program

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -2,9 +2,21 @@ package com.test.test;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		System.out.println("Created a Baseclass...");
-
+	public static void main(String[] args) 
+	{
+	int num=0,sum=0, n=1234;
+	
+	while(n!=0)
+	{
+		num=n%10;
+		sum=sum+num;
+		n=n/10;
+		
+		
 	}
+		System.out.println(sum);
+		
+	}
+
 
 }
