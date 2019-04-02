20927e47528de87a34ebea076507b7abd5a10c4d
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-04-02 08:38:13 +0530

    Modified the baseClass

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -2,9 +2,22 @@ package com.test.test;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		System.out.println("Created a Baseclass...");
-
+	public BaseClass() {
+		
 	}
+	
+	public void launchBrowser() {
+		System.out.println("Added launch browser method");
+	}
+	
+	public void selectDropdown() {
+		System.out.println("Added select Dropdown method");
+	}
+	
+	public void selectCheckbox() {
+		System.out.println("Added select Checkbox method");
+	}
+	
+	
 
 }
