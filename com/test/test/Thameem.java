bf9152acfb9a192954cb6ac7bc50062047ad3214
jaffarsadique <jaffy71@gmail.com>
2019-04-02 15:16:56 +0530

    was added

--- com/test/test/Thameem.java
+++ com/test/test/Thameem.java
@@ -2,7 +2,7 @@ package com.test.test;
 
 public class Thameem {
 	public static void main(String[] args) {
-		String str = "Project class started";
+		String str = "Project class was started";
 		char[] ch = str.toCharArray();
 		for (int i = 0; i < ch.length; i++) {
 			int count = 0;
