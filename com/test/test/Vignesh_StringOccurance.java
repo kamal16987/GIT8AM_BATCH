24b4a6ec37d926c7ece4bf449f7baa1334ed61c6
VigneshKannadasan <vigneshkannadasan7@gmail.com>
2019-04-02 13:17:23 +0530

    Branch 3 were been added

--- com/test/test/Vignesh_StringOccurance.java
+++ com/test/test/Vignesh_StringOccurance.java
@@ -15,12 +15,16 @@ public class Vignesh_StringOccurance {
 	public void branchTwo() {
 		System.out.println("Branch 2 value were been added");
 	}
+	public void branchThree() {
+		System.out.println("Branch 3 value were been added");
+	}
 
 	public static void main(String[] args) {
 	Vignesh_StringOccurance v= new Vignesh_StringOccurance();
 		v.display();
 		v.branchOne();
 		v.branchTwo();
+		v.branchThree();
 	Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string to find string occurance");
 	    String s = sc.next();											
