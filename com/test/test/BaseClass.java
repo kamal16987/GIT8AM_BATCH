eb49aee2771c10cb2f9f4be3d3be74595fed1a3a
Your Name <shatishkumar17@gmail.com>
2019-04-02 21:39:15 +0530

    trying to resolve the merge conflict

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -1,9 +1,20 @@
 package com.test.test;
 
 public class BaseClass {
+	
+	private void mergeConflict() {
+		 System.out.println("trying to resolve the merge conflict");
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Created a Baseclass...");
+		
+		BaseClass print = new BaseClass();
+		
+		print.mergeConflict();
+		
+		
 
 	}
 
