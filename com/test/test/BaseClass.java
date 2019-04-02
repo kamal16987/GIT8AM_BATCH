8aff556a872eb43d674f79de1b7bd0ea554d1c86
mani6726 <mani6726@gmail.com>
2019-04-02 20:19:18 +0530

    Deleted the method in base class

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -2,17 +2,10 @@ package com.test.test;
 
 public class BaseClass {
 	
-	public void method()
-	{
-		System.out.println("hello");
-	}
-
 	public static void main(String[] args) {
-		
-BaseClass bd=new BaseClass();
-		bd.method();
+	
 	System.out.println("modified");	
-
+System.out.println("deleted the method");
 }
 
 }
