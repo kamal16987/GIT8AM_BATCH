42f2d44529cfbc9aef30226ec454e7e2840d1f7b
shravan2412 <shravankumar241207@gmail.com>
2019-04-03 10:09:51 +0530

    new class edited

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -2,26 +2,17 @@ package com.test.test;
 
 public class BaseClass {
 
-	
+	public static void main(String[] args) {
+		int num = 0, sum = 0, n = 1234;
 
+		while (n != 0) {
+			num = n % 10;
+			sum = sum + num;
+			n = n / 10;
 
-	public static void main(String[] args) 
-	{
-	int num=0,sum=0, n=1234;
-	
-	while(n!=0)
-	{
-		num=n%10;
-		sum=sum+num;
-		n=n/10;
-		
-		
-	}
+		}
 		System.out.println(sum);
-		
+		System.out.println(sum);
 	}
-
-
-
 
 }
