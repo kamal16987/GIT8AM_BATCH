fb7918c48be59fd02422c4039e7a7ef6f9d57ebe
Nivethaa <nivethaasrini@gmail.com>
2019-04-03 10:06:35 +0530

    String reverse without using inbuilt functions added

--- com/test/test/Nivethaa.java
+++ com/test/test/Nivethaa.java
@@ -21,10 +21,11 @@ public class Nivethaa {
 			System.out.println(i);
 		}
 
-		StringBuilder str2 = new StringBuilder();
-		str2.append(s);
-		str2 = str2.reverse();
-		System.out.println(str2);
+		char[] char1= s.toCharArray();
+		System.out.println("Reverse String : ");
+		for (int i = char1.length-1; i >= 0; i--) {
+			System.out.print(char1[i]);
+		}
 
 	}
 
