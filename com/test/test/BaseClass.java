9e5c36b84f0d0d1136e3798b06da1bf08037c4af
banu <bowsiya22a@gmail.com>
2019-04-03 10:22:22 +0530

    class edited

--- com/test/test/BaseClass.java
+++ com/test/test/BaseClass.java
@@ -1,18 +1,20 @@
 package com.test.test;
 
 public class BaseClass {
+	
+	public void method()
+	{
+		System.out.println("hello");
+	}
 
 	public static void main(String[] args) {
-		int num = 0, sum = 0, n = 1234;
+		
+BaseClass bd=new BaseClass();
+		bd.method();
+	System.out.println("modified");	
+	
+	System.out.println("bbannu");
 
-		while (n != 0) {
-			num = n % 10;
-			sum = sum + num;
-			n = n / 10;
-
-		}
-		System.out.println(sum);
-		System.out.println(sum);
-	}
+}
 
 }
