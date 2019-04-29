ba9ade7a0bbb946acb222480d2c4b42c175a1876
karthirajan <kajekarthi@gmail.com>
2019-04-29 12:35:35 +0530

    add

---
+++ com/test/test/Manik1294.java
@@ -0,0 +1,31 @@
+package com.test.test;
+
+public class Manik1294 {
+	public void company() {
+		
+
+	}
+	public void company(int a,int b) {
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+	public void company(float c, float d ) {
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+	public void company(String e,String f) {
+		System.out.println(e);
+		System.out.println(f);
+		
+	}
+	public static void main(String[] args) {
+		Manik1294 cd=new Manik1294();
+		cd.company(21,34);
+		cd.company(21f,12f);
+		cd.company("mani","raj");
+	}
+
+}
